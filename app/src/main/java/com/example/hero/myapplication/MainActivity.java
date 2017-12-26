d293171f10b04423bc816de68b798fa9fa1a9e22
chaimard <chaimard@email.krirk.ac.th>
2017-12-26 12:08:21 +0700

    Finish 26Dec

--- app/src/main/java/com/example/hero/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hero/myapplication/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.hero.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.hero.myapplication.fragment.mainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new mainFragment())
+                    .commit();
+
+
+        }
+
+
+    }   //Main Method
+
+
+}   //Main class
---
+++ app/src/main/java/com/example/hero/myapplication/fragment/mainFragment.java
@@ -0,0 +1,31 @@
+package com.example.hero.myapplication.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hero.myapplication.R;
+
+/**
+ * Created by hero on 26/12/2560.
+ */
+
+public class mainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return view;
+
+    }
+
+
+}   //Main Class
